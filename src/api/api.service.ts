a155cb72600dc920653d825d1e21d864d547e32e
Covenant Ogowale <justcovenant@outlook.com>
2024-07-01 02:38:22 +0100

    test: testing the api response

--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -11,19 +11,17 @@ export class ApiService {
     const latitude = geoData.data.latitude;
     const longitude = geoData.data.longitude; 
 
-    const temperature = await axios.get('https://api.open-meteo.com/v1/forecast', {
-      data: {
-        latitude: latitude,
-        longitude: longitude,
-        hourly: 'temperature_2m',
-      }
-    })
+    try {
+      const temperature = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${process.env.OPEN_WEATHER_API_KEY}`)
 
-    return {
-      client_ip: ip,
-      location: geoData.data.city,
-      temperature,
-      greeting: `Hello, ${(query.visitor_name).replace('\" || \'', "")}, the temperature is ${temperature} degrees Celcius in New York`,
+      return {
+        client_ip: ip,
+        location: geoData.data.city,
+        temperature,
+        greeting: `Hello, ${(query.visitor_name).replace('\" || \'', "")}, the temperature is ${temperature.data.hourly[0].temp} degrees Celcius in New York`,
+      }
+    }catch(err) {
+      console.log(err)
     }
   }
 }
