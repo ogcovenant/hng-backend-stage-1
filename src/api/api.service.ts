3941c9f59c7f9be71c22770585d111e3b2199325
Covenant Ogowale <justcovenant@outlook.com>
2024-07-01 02:11:44 +0100

    test: testing the api response

--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -1,17 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { RequestDTO } from './api.dto';
 import axios from 'axios';
+import { fetchWeatherApi } from 'openmeteo';
 
 @Injectable()
 export class ApiService {
   async hello(query: RequestDTO, ip: string){
 
-    const apiResponse = await axios.get(`https://get.geojs.io/v1/ip.json`);
+    const geoData = await axios.get(`https://get.geojs.io/v1/ip/geo/${ip}.json`);
+
+    const latitude = geoData.data.latitude;
+    const longitude = geoData.data.longitude; 
+
+    const temperature = await fetchWeatherApi('https://api.open-meteo.com/v1/forecast', {
+      latitude,
+      longitude
+    })
 
     return {
       client_ip: ip,
-      location: apiResponse.data,
-      greeting: `Hello ${query.visitor_name}`,
+      location: geoData.data.city,
+      temperature,
+      greeting: `Hello, ${(query.visitor_name).replace('\" || \'', "")}, the temperature is ${temperature} degrees Celcius in New York`,
     }
   }
 }
